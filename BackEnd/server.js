2746f2d0a5a3d95a203afb2ffc244f3586505747
Priya190104 <ardhana332@gmai.com>
2026-02-08 22:22:50 +0700

    feat: replace map picker with coordinate inputs + update logos (favicon, logo192, logo512)

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -16,6 +16,8 @@ const corsOptions = {
     'http://localhost:3001',
     'http://127.0.0.1:3000',
     'http://202.10.45.79:3000',
+    'http://202.10.45.79',
+    'http://202.10.45.79:80',
     process.env.FRONTEND_URL
   ].filter(Boolean),
   credentials: true,
